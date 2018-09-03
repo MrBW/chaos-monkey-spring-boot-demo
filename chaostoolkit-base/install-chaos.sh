a5baf32beb62d613793f6c6267e2826d4200cab4
benjamin.wilms <2GenUsr74>
2018-09-03 09:49:21 +0200

    base

---
+++ chaostoolkit-base/install-chaos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo C.UTF exports
+echo Install python3
+apt-get -y install python3 python3-venv
+echo Install pkill openssl
+apt-get -y install procps
+apt-get -y install openssl
+echo Create a virtual environment
+python3 -m venv ~/.venvs/chaostk
+source  ~/.venvs/chaostk/bin/activate
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+echo Install Chaos Toolkit
+pip install chaostoolkit
+echo Check Chaos Toolkit version
+chaos --version
+echo Install driver Spring Boot
+pip install -U chaostoolkit-spring
