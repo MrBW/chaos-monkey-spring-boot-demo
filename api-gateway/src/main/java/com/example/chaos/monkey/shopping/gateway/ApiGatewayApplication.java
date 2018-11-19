e87ee51bf1d0c87b3298ba2c371f88e8ff6d9995
Benjamin Wilms <benjamin.wilms@codecentric.de>
2018-11-19 13:03:07 +0100

    add Eureka-Server for Discovery Service integration

--- api-gateway/src/main/java/com/example/chaos/monkey/shopping/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/chaos/monkey/shopping/gateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.example.chaos.monkey.shopping.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
