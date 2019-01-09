e90665ba5cc2182c1c0651dafe69c64e58a52dbf
Benjamin Wilms <benjamin.wilms@codecentric.de>
2019-01-09 15:19:47 +0100

    extended talk @ foconis ag

--- gateway/src/main/java/com/example/chaos/monkey/shopping/gateway/StartPageController.java
+++ gateway/src/main/java/com/example/chaos/monkey/shopping/gateway/StartPageController.java
@@ -10,19 +10,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.support.TimeoutException;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.ClientResponseExtensionsKt;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 
 /**
- * @author Ryan Baxter
+ * @author Ryan Baxter, Benjamin Wilms
  */
 @RestController
 public class StartPageController {
@@ -36,6 +41,8 @@ public class StartPageController {
     @Value("${rest.endpoint.hotdeals}")
     private String urlHotDeals;
 
+    private RestTemplate restClient;
+
     private ProductResponse errorResponse;
     private WebClient webClient;
     private Tracer tracer;
@@ -43,44 +50,38 @@ public class StartPageController {
     public StartPageController(WebClient webClient, Tracer tracer) {
         this.webClient = webClient;
         this.tracer = tracer;
+        this.restClient = new RestTemplate();
 
         this.errorResponse = new ProductResponse();
         errorResponse.setResponseType(ResponseType.ERROR);
         errorResponse.setProducts(Collections.emptyList());
     }
 
-    private ParameterizedTypeReference<Product> productParameterizedTypeReference =
-            new ParameterizedTypeReference<Product>() {
-            };
 
-    private Function<ClientResponse, Mono<ProductResponse>> responseProcessor = clientResponse -> {
-        HttpHeaders headers = clientResponse.headers().asHttpHeaders();
-
-        if (headers.containsKey("fallback") && headers.get("fallback").contains("true")) {
-            this.tracer.currentSpan().tag("failure","fallback");
-
-            return Mono.just(new ProductResponse(ResponseType.FALLBACK, Collections.emptyList()));
-
-        } else if (clientResponse.statusCode().isError()) {
-            this.tracer.currentSpan().tag("failure","error");
-            // HTTP Error Codes are not handled by Hystrix!?
-            return Mono.just(new ProductResponse(ResponseType.ERROR, Collections.emptyList()));
+    @RequestMapping(value = {"/startpage", "/startpage/{version}"}, method = RequestMethod.GET)
+    public Mono<Startpage> delegateStartpageRequest(@PathVariable Optional<String> version) {
+        if (version.isPresent()) {
+            if (version.get().equalsIgnoreCase("cb")) {
+                return getStartpageCircuitBreaker();
+            } else if (version.get().equalsIgnoreCase("lb")) {
+                return getStartpageLoadBalanced();
+            }
         }
+        //default landing
+        return getStartpageLegacy();
 
-        return clientResponse.bodyToFlux(productParameterizedTypeReference).collectList()
-                .flatMap(products -> Mono.just(new ProductResponse(ResponseType.REMOTE_SERVICE, products)));
-    };
+    }
 
 
-    @GetMapping("/startpage")
-    public Mono<Startpage> getStartpage() {
+    private Mono<Startpage> getStartpageCircuitBreaker() {
         long start = System.currentTimeMillis();
 
-        Span newSpan = this.tracer.nextSpan().name("getAllProducts");
+        Span newSpan = this.tracer.nextSpan().name("allProductsCircuitBreaker");
+        newSpan.tag("circuit.breaker", "true");
+        newSpan.tag("load-balanced", "false");
+
         try (Tracer.SpanInScope ws = this.tracer.withSpanInScope(newSpan.start())) {
 
-            newSpan.annotate("startpage");
-            newSpan.tag("request", "startpage");
             Mono<ProductResponse> hotdeals = webClient.get().uri("/hotdeals").exchange().flatMap(responseProcessor)
                     .doOnError(t -> {
                         System.out.println("on error");
@@ -107,7 +108,7 @@ public class StartPageController {
                         return Mono.just(errorResponse);
                     });
 
-            return aggregateResults(start, hotdeals, fashionBestSellers, toysBestSellers);
+            return aggregateResults(start, hotdeals, fashionBestSellers, toysBestSellers, newSpan);
         } finally {
             newSpan.finish();
         }
@@ -116,30 +117,74 @@ public class StartPageController {
     }
 
 
-    @GetMapping("/startpage/lb")
-    public Mono<Startpage> getStartpageRetry() {
+    private Mono<Startpage> getStartpageLoadBalanced() {
         long start = System.currentTimeMillis();
-        Mono<ProductResponse> hotdeals = webClient.get().uri("/lb/hotdeals").exchange().flatMap(responseProcessor)
-                .onErrorResume(t -> {
-                    t.printStackTrace();
-                    return Mono.just(errorResponse);
-                });
-        Mono<ProductResponse> fashionBestSellers = webClient.get().uri("/lb/fashion/bestseller").exchange().flatMap(responseProcessor)
-                .onErrorResume(t -> {
-                    t.printStackTrace();
-                    return Mono.just(errorResponse);
-                });
-        Mono<ProductResponse> toysBestSellers = webClient.get().uri("/lb/toys/bestseller").exchange().flatMap(responseProcessor)
-                .onErrorResume(t -> {
-                    t.printStackTrace();
-                    return Mono.just(errorResponse);
-                });
+
+        Span newSpan = this.tracer.nextSpan().name("allProductsLoadBalanced");
+        newSpan.tag("load.balanced", "true");
+        newSpan.tag("circuit.breaker", "true");
+
+        try (Tracer.SpanInScope ws = this.tracer.withSpanInScope(newSpan.start())) {
+            Mono<ProductResponse> hotdeals = webClient.get().uri("/lb/hotdeals").exchange().flatMap(responseProcessor)
+                    .onErrorResume(t -> {
+                        t.printStackTrace();
+                        return Mono.just(errorResponse);
+                    });
+            Mono<ProductResponse> fashionBestSellers = webClient.get().uri("/lb/fashion/bestseller").exchange().flatMap(responseProcessor)
+                    .onErrorResume(t -> {
+                        t.printStackTrace();
+                        return Mono.just(errorResponse);
+                    });
+            Mono<ProductResponse> toysBestSellers = webClient.get().uri("/lb/toys/bestseller").exchange().flatMap(responseProcessor)
+                    .onErrorResume(t -> {
+                        t.printStackTrace();
+                        return Mono.just(errorResponse);
+                    });
 
 
-        return aggregateResults(start, hotdeals, fashionBestSellers, toysBestSellers);
+            return aggregateResults(start, hotdeals, fashionBestSellers, toysBestSellers, newSpan);
+        } finally {
+            newSpan.finish();
+        }
     }
 
-    private Mono<Startpage> aggregateResults(long start, Mono<ProductResponse> hotdeals, Mono<ProductResponse> fashionBestSellers, Mono<ProductResponse> toysBestSellers) {
+    private Mono<Startpage> getStartpageLegacy() {
+
+        Span newSpan = this.tracer.nextSpan().name("allProductsLegacy");
+        newSpan.tag("circuit.breaker", "false");
+        newSpan.tag("load.balanced", "false");
+
+        try (Tracer.SpanInScope ws = this.tracer.withSpanInScope(newSpan.start())) {
+
+            Startpage page = new Startpage();
+
+            long start = System.currentTimeMillis();
+
+            // Get Responses from remote services
+            page.setFashionResponse(getProductResponse(urlFashion));
+            page.setToysResponse(getProductResponse(urlToys));
+            page.setHotDealsResponse(getProductResponse(urlHotDeals));
+
+            // Summary
+            page.setStatusFashion(page.getFashionResponse().getResponseType().name());
+            page.setStatusToys(page.getToysResponse().getResponseType().name());
+            page.setStatusHotDeals(page.getHotDealsResponse().getResponseType().name());
+
+            // Request duration
+            page.setDuration(System.currentTimeMillis() - start);
+
+            newSpan.tag("product.size.fashion", String.valueOf(page.getFashionResponse().getProducts().size()));
+            newSpan.tag("product.size.toys", String.valueOf(page.getToysResponse().getProducts().size()));
+            newSpan.tag("product.size.hotdeals", String.valueOf(page.getHotDealsResponse().getProducts().size()));
+
+            return Mono.just(page);
+        } finally {
+            newSpan.finish();
+        }
+
+    }
+
+    private Mono<Startpage> aggregateResults(long start, Mono<ProductResponse> hotdeals, Mono<ProductResponse> fashionBestSellers, Mono<ProductResponse> toysBestSellers, Span newSpan) {
         Mono<Startpage> page = Mono.zip(hotdeals, fashionBestSellers, toysBestSellers).flatMap(t -> {
             Startpage p = new Startpage();
             ProductResponse deals = t.getT1();
@@ -154,8 +199,46 @@ public class StartPageController {
             // Request duration
             p.setDuration(System.currentTimeMillis() - start);
 
+            newSpan.tag("product.size.fashion", String.valueOf(fashion.getProducts().size()));
+            newSpan.tag("product.size.toys", String.valueOf(toys.getProducts().size()));
+            newSpan.tag("product.size.hotdeals", String.valueOf(deals.getProducts().size()));
+
             return Mono.just(p);
         });
         return page;
     }
+
+
+    private ProductResponse getProductResponse(String url) {
+        ProductResponse response = new ProductResponse();
+
+        response.setProducts(restClient.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<Product>>() {
+        }).getBody());
+
+        response.setResponseType(ResponseType.REMOTE_SERVICE);
+
+        return response;
+    }
+
+    private ParameterizedTypeReference<Product> productParameterizedTypeReference =
+            new ParameterizedTypeReference<Product>() {
+            };
+
+    private Function<ClientResponse, Mono<ProductResponse>> responseProcessor = clientResponse -> {
+        HttpHeaders headers = clientResponse.headers().asHttpHeaders();
+
+        if (headers.containsKey("fallback") && headers.get("fallback").contains("true")) {
+            this.tracer.currentSpan().tag("failure", "fallback");
+
+            return Mono.just(new ProductResponse(ResponseType.FALLBACK, Collections.emptyList()));
+
+        } else if (clientResponse.statusCode().isError()) {
+            this.tracer.currentSpan().tag("failure", "error");
+            // HTTP Error Codes are not handled by Hystrix!?
+            return Mono.just(new ProductResponse(ResponseType.ERROR, Collections.emptyList()));
+        }
+
+        return clientResponse.bodyToFlux(productParameterizedTypeReference).collectList()
+                .flatMap(products -> Mono.just(new ProductResponse(ResponseType.REMOTE_SERVICE, products)));
+    };
 }
