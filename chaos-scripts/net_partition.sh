6b8e7583d11698ca5b89fd9d5f4ff1e47e821a2f
Benjamin Wilms <benjamin.wilms@codecentric.de>
2019-03-11 15:51:36 +0100

    hostpattern

--- chaos-scripts/net_partition.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-IFS=', ' read -r -a hosts <<< "$1"
-iptables -I INPUT -s ${hosts[$2]} -j DROP
-iptables -I OUTPUT -d ${hosts[$2]} -j DROP
--- chaos-scripts/net_partition_remove.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-IFS=', ' read -r -a hosts <<< "$1"
-iptables -D INPUT -s ${hosts[$2]} -j DROP
-iptables -D OUTPUT -d ${hosts[$2]} -j DROP
